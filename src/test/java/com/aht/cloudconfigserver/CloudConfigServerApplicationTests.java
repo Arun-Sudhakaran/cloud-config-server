f6e0e97e439990c9ee481d8b44580c6562b8d63c
Arun-Sudhakaran <arunsudhakaranfuturetechnology@gmail.com>
2022-11-03 21:41:06 +0530

    Initial commit

---
+++ src/test/java/com/aht/cloudconfigserver/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aht.cloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
